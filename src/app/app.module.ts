402c013fdf205b5430ac8722e67c6e682aae6dfb
le314u <lucsmfpiu@gmail.com>
2020-07-21 23:40:23 -0230

    Init Projeto c/ tela Tipo produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TipoProdutoComponent } from './tipo-produto/tipo-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipoProdutoComponent
   ],
   imports: [
     BrowserModule
